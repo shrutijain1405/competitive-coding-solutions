56c88351b2dd317606dc5811d070fbd3bf882e49
shruti jain <shrutipraveenjain@gmail.com>
2020-08-19 17:30:18 +0530

    made the code more efficient

--- searchMatrix.cpp
+++ searchMatrix.cpp
@@ -31,6 +31,7 @@ Output: false
 using namespace std;
 class Solution {
 public:
+    /**
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
     	if(matrix.empty()==true)
             return false;
@@ -57,12 +58,50 @@ public:
             return flag;
         }
     }
+    **/
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty()==true)
+            return false;
+        else{
+            bool flag = false;
+            int n = matrix[0].size(); //no of cols
+            int m = matrix.size(); //no of rows
+            bool foundRange = false;
+            int i =0;
+            for(i =0;i<m; i++){
+                if(target > matrix[i][0] && target < matrix[i][n-1]){
+                    foundRange = true;
+                    break;
+                }
+                else if(target == matrix[i][0] || target == matrix[i][n-1])
+                    return true;
+            }
+            if(foundRange == false)
+                return false;
+            int lb = 0;
+            int ub = n-1;
+            int mid = (lb+ub)/2;
+            while(lb<=ub){
+                mid = (lb+ub)/2;
+                if(matrix[i][mid] > target)
+                    ub = mid-1;
+                else if(matrix[i][mid] < target)
+                    lb = mid + 1;
+                else{
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+
+        }
+    }
 };
 int main(){
 	Solution ob;
 	//vector< vector<int> > matrix = {};
 	vector< vector<int> > matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,50}};
-	bool ans = ob.searchMatrix(matrix, 100);
+	bool ans = ob.searchMatrix(matrix, 5);
 	cout<<"ans = "<<ans<<endl;
 	return 0;
 }
